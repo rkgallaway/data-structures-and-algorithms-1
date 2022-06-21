d74d8b48041f1839629c1e4d9d4340ae16cd906b
DYLAN ULLRICH <dylan@getullrichordietrying.com>
2022-06-20 21:12:33 -0700

    code challenge 5

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,9 @@
+# Linked Lists
+
+## Whiteboard Process
+
+![Linked Lists](array-insert-shift.png)
+
+## Approach & Efficiency
+
+## Collaborators: **Dylan Ullrich**
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,5 +1,44 @@
 'use strict';
 
-class LinkedList {}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-module.exports = LinkedList;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    this.head = new Node(value, this.head);
+  }
+
+  includes(val) {
+    let current = this.head;
+    // console.log(searchVal);
+    // console.log(current.value);
+    while (current.value !== val) {
+      current = current.next;
+    }
+    console.log(`Value: ${val} is present`);
+  }
+
+  toString(value) {
+    console.log(`{ ${this.head.value} } ->, Null`);
+  }
+}
+
+let linkedList = new LinkedList();
+
+linkedList.insert(99);
+linkedList.includes(99);
+linkedList.toString(linkedList);
+console.log(linkedList);
+
+module.exports = {
+  Node,
+  LinkedList,
+};
